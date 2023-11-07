66357960e4f261b0c7dd5c3a96b8e109e5f11cc8
KillerWord <115478386+MaCheNeSohh@users.noreply.github.com>
2023-11-07 20:34:50 +0100

    Update script.js
    Signed-off-by: KillerWord <115478386+MaCheNeSohh@users.noreply.github.com>

--- script.js
+++ script.js
@@ -24,19 +24,16 @@ function generaFrasi(opzione) {
     var input13 = document.getElementById('input13').value;
     var frasiGenerare = "";
     var frasiGenerare2 = "";
-    var frasiGenerare3 = "";
     var frasiGenerare4 = "";
     var frasiGenerare5 = "";
     var frasiGenerare6 = "";
     var frasiGenerare7 = "";
-    var frasiGenerare8 = "";
     var frasiGenerare9 = "";
     var frasiGenerare10 = "";
     var frasiGenerare11 = "";
     var frasiGenerare12 = "";
     var frasiGenerare13 = "";
     var frasiGenerare14 = "";
-    var frasiGenerare15 = "";
     var frasiGenerare16 = "";
     var frasiGenerare17 = "";
     var frasiGenerare18 = "";
@@ -64,7 +61,6 @@ function generaFrasi(opzione) {
 if (opzione === 1) {
     frasiGenerare = "Il sottoscritto KillerWord in quanto officiante abilitato dallo stato di Metropolis, dichiara " + input1 + " e " + input2 + " civilmente uniti ai sensi della legge sulle unioni civili del 05/10/2020" + ".";
     frasiGenerare2 = "/marry marry " + input1 + " " + input2;
-    frasiGenerare3 = "/scontrino Segreteria 2500 " + input1 + " Unione Civile";
     frasiGenerare4 = "/Unione civile " + input1 + " " + input2;
     frasiGenerare5 = "Il sottoscritto, in quanto officiante abilitato dallo stato di Metropolis, dichiara " + input1 + " e " + input2 + " civilmente uniti in data " + input3 + " ai sensi della legge sulle unioni civili del 05/10/2020" + ".";
     frasiGenerare6 = "Si attesta inoltre, la presenza all'atto dell'unione, dei testimoni " + input4 + " e " + input5 + ".";
@@ -73,7 +69,6 @@ if (opzione === 1) {
 }
 
 else if (opzione === 2) {
-    frasiGenerare8 = "/scontrino Segreteria 1000 " + input6 + " Divorzio";
     frasiGenerare9 = "Il sottoscritto, in quanto officiante abilitato dallo stato di Metropolis, dichiara: " + input6 + " e " + input7 + " divorziati ai sensi della legge sulle unioni civili del 05/10/2020.";
     frasiGenerare10 = "Cittadino 1: " + input6 + " Cittadino 2: " + input7 + " Officiante: KillerWord " + " Data e Ora: " + input8;  
     frasiGenerare11 = "/marry divorce " + input6 + " oppure " + input7;
@@ -83,7 +78,6 @@ else if (opzione === 2) {
 }
 
 if (opzione === 3) {
-    var frasiGenerare15 = "/scontrino segreteria " + risultato + " " + input9 + " Registrazione Famiglia/Rinnovo Famiglia " + input10;
     var frasiGenerare16 = "Nome Famiglia: " + input10 + " N. Capofamiglia: " + input9 + " Componenti: " + input11 + " Data di registrazione: " + input13 + " Data di scadenza: " + input99 + " (la data potrebbe non essere corretta)";
     var frasiGenerare17 = " Con questo documento la segreteria di Metropolis attesta che la seguente famiglia " + input10 + " con a capo " + input9 + " la quale presenta " + input12 + " membri " + " è riconosciuta dallo Stato.";
     var frasiGenerare18 = " Data di rilascio: " + input13 + " Firma segretario: KillerWord " + " Firma CapoFamiglia: " + input9 + " Data di scadenza: " + input99 + " (la data potrebbe non essere corretta)";
@@ -94,19 +88,16 @@ if (opzione === 3) {
 
     document.getElementById('frasiGenerare').textContent = frasiGenerare;
     document.getElementById('frasiGenerare2').textContent = frasiGenerare2;
-    document.getElementById('frasiGenerare3').textContent = frasiGenerare3;
     document.getElementById('frasiGenerare4').textContent = frasiGenerare4;
     document.getElementById('frasiGenerare5').textContent = frasiGenerare5;
     document.getElementById('frasiGenerare6').textContent = frasiGenerare6;
     document.getElementById('frasiGenerare7').textContent = frasiGenerare7;
-    document.getElementById('frasiGenerare8').textContent = frasiGenerare8;
     document.getElementById('frasiGenerare9').textContent = frasiGenerare9;
     document.getElementById('frasiGenerare10').textContent = frasiGenerare10;
     document.getElementById('frasiGenerare11').textContent = frasiGenerare11;
     document.getElementById('frasiGenerare12').textContent = frasiGenerare12;
     document.getElementById('frasiGenerare13').textContent = frasiGenerare13;
     document.getElementById('frasiGenerare14').textContent = frasiGenerare14;
-    document.getElementById('frasiGenerare15').textContent = frasiGenerare15;
     document.getElementById('frasiGenerare16').textContent = frasiGenerare16;
     document.getElementById('frasiGenerare17').textContent = frasiGenerare17;
     document.getElementById('frasiGenerare18').textContent = frasiGenerare18;
